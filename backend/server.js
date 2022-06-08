63ef266a86f7b4f8a71f7c7001887da5ad3a9181
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-08 09:31:01 -0700

    Added : cors to backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
+// filter the website that can connect to
+const options = {
+  origin: 'http://localhost:3000',
+  useSuccessStatus: 200,
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('welcome from home');
@@ -9,13 +15,9 @@ app.get('/books', (req, res) => {
   res.send('book');
 });
 app.listen(8000, () => {
-  console.log('server isS lestining...');
+  console.log('server is listening...');
 });
 
-
-
-
-
-
-
 // add nodemon npm i nodemon
+// add cors , npm i cors
+// then import cors

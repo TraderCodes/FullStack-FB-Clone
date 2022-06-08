87af0dfeb876e41806023c6e488baeb284a9bb39
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-08 09:59:37 -0700

    add dynamic routes by using readdirsync

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { readdirSync } = require('fs'); //read files in the folder
+ 
 // filter the website that can connect to
 const options = {
   origin: 'http://localhost:3000',
@@ -8,12 +10,12 @@ const options = {
 };
 app.use(cors(options));
 
-app.get('/', (req, res) => {
-  res.send('welcome from home');
-});
-app.get('/books', (req, res) => {
-  res.send('book');
-});
+
+// app.use('/', useRoutes) ✔ instead of using this we use below ; 
+// require('./routes/'+r) is everyfile name we set to 
+readdirSync('./routes').map((r) => app.use('/',require('./routes/'+r)) )
+
+
 app.listen(8000, () => {
   console.log('server is listening...');
 });
@@ -21,3 +23,10 @@ app.listen(8000, () => {
 // add nodemon npm i nodemon
 // add cors , npm i cors
 // then import cors
+
+// app.get('/', (req, res) => {
+//   res.send('welcome from home');
+// });
+// app.get('/books', (req, res) => {
+//   res.send('book');
+// });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,9 @@
 function App() {
+  const get = async() => {
+  //  awaits for the response then the next code runs
+    const  res = await fetch('http://localhost:8000');
+    console.log(res)
+  };
   return <div>welcome to frontend</div>;
 }
 

770177c29b6d31fa7d0322d8419d3decad983cf9
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-16 07:06:56 -0700

    Added : Html to login Page and install formik

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,5 +1,40 @@
+// install formail and yup
+import { Formik, Form } from 'formik';
+import { Link } from 'react-router-dom';
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <div className="login_wrapper">
+        <div className="login_wrap">
+          {/* //login 1 = LOGO🟢  // loging 2 = signup 🟢*/}
+          <div className="login_1">
+            <img src="../../icons/facebook.svg" alt="" />
+            <span>Facebook Text area</span>
+          </div>
+          <div className="login_2">
+            <div className="login_2_wrap">
+              <Formik>
+                {(Formik) => (
+                  <Form>
+                    <input type="text" />
+                    <input type="text" />
+                    <button type="submit">Log In</button>
+                  </Form>
+                )}
+              </Formik>
+              <Link to="/forgot">Forgot password</Link>
+              {/* splitter */}
+              <div className="sign_splitter"></div>
+              <button className="blue_btn open_signup">Create Account</button>
+            </div>
+            {/* Create a page text */}
+            <Link to="/">
+              <b>Create a page</b> for a celebrity,brand or business
+            </Link>
+          </div>
+        </div>
+        <div className="register"> </div>
+      </div>
+    </div>
+  );
 }

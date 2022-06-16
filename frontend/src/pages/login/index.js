3afed3af401fe618e07344c9b155731fc2ddbaf7
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-16 08:50:41 -0700

    Setup Input components / css

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -2,6 +2,7 @@
 import './style.css';
 import { Formik, Form } from 'formik';
 import { Link } from 'react-router-dom';
+import LoginInput from '../../components/inputs/logininput';
 export default function Login() {
   return (
     <div className="login">
@@ -10,26 +11,34 @@ export default function Login() {
           {/* //login 1 = LOGO🟢  // loging 2 = signup 🟢*/}
           <div className="login_1">
             <img src="../../icons/facebook.svg" alt="" />
-            <span>Facebook Text area</span>
+            <span>
+              Facebook Clone Created by Jamal Many hours of dedication
+            </span>
           </div>
           <div className="login_2">
             <div className="login_2_wrap">
               <Formik>
                 {(Formik) => (
                   <Form>
-                    <input type="text" />
-                    <input type="text" />
+                    <LoginInput placeholder="Email Address" />
+                    <LoginInput placeholder="Password" />
                     <button type="submit" className="blue_btn">
                       Log In
                     </button>
                   </Form>
                 )}
               </Formik>
-              <Link to="/forgot" className='forgot_password'>Forgot password ?</Link>
+              <Link to="/forgot" className="forgot_password">
+                Forgot password ?
+              </Link>
               {/* splitter */}
               <div className="sign_splitter"></div>
-              <button className="blue_btn open_signup" id='demo'>Demo Login</button>
-              <button className="blue_btn open_signup">Create Account</button>
+              <button className="blue_btn open_signup" id="demo">
+                Demo Login
+              </button>
+              <button className="blue_btn open_signup">
+                Create NewAccount
+              </button>
             </div>
             {/* Create a page text */}
             <Link to="/" className="sign_extra">

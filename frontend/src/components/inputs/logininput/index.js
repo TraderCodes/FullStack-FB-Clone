6059ae6e9bbf536a4a1c5eed975b57a18d81aa81
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-06-16 11:07:48 -0700

    Fixed : warning error on icons

--- frontend/src/components/inputs/logininput/index.js
+++ frontend/src/components/inputs/logininput/index.js
@@ -5,21 +5,22 @@ export default function LoginInput({ placeholder, ...props }) {
   const [field, meta] = useField(props);
   return (
     <div className="input_wrap">
-      <div>
+      {/* error box  */}
+      <div className="input_error"> 
         {/* change message from yup input  in index.js */}
         {meta.touched && meta.error && <ErrorMessage name={field.name} />}
       </div>
       {/* pass in placeholder from login/index.js */}
       <input
         // 🔴 Change input border style
-        className={meta.touched && meta.error && 'input_error_border'}
+        className={meta.touched && meta.error ? 'input_error_border':''}
         type={field.type}
         name={field.name}
         placeholder={placeholder}
         {...field}
         {...props}
       />
-      {meta.touched && meta.error && <i className='info_icon'></i> }
+      {meta.touched && meta.error && <i className="info_icon"></i>}
     </div>
   );
 }

ae1114b6e374bec59c1c9a53f79fd16c917aa484
TraderCodes <TraderCodes@users.noreply.github.com>
2022-06-18 09:54:21 -0700

    gender css

--- frontend/src/components/login/RegisterForm.js
+++ frontend/src/components/login/RegisterForm.js
@@ -80,14 +80,38 @@ export default function RegisterForm() {
                 <div className="reg_line_header">
                   Gender <i className="info_icon"></i>
                 </div>
-                <div className='reg_grid'>
+                <div className="reg_grid">
                   <label htmlFor="male">
                     Male
-                    <input type="radio" name="gender" id="male" value='male' onChange={handleRegisterChange}/>
+                    <input
+                      type="radio"
+                      name="gender"
+                      id="male"
+                      value="male"
+                      onChange={handleRegisterChange}
+                    />
+                  </label>
+                  <label htmlFor="female">
+                    Female
+                    <input
+                      type="radio"
+                      name="gender"
+                      id="female"
+                      value="female"
+                      onChange={handleRegisterChange}
+                    />
+                  </label>
+                  <label htmlFor="I decide">
+                    I Decide
+                    <input
+                      type="radio"
+                      name="gender"
+                      id="custom"
+                      value="custom"
+                      onChange={handleRegisterChange}
+                    />
                   </label>
                 </div>
-
-
               </div>
             </Form>
           )}

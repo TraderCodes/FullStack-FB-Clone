3f30e68df8597f45318d369feafeef2769c54c61
TraderCodes <TraderCodes@users.noreply.github.com>
2022-06-20 11:45:22 -0700

    added Profile css

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -2,19 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
 import {
+  ArrowDown,
   Friends,
   Gaming,
   HomeActive,
   Logo,
   Market,
+  Menu,
+  Messenger,
+  Notifications,
   Search,
   Watch,
 } from '../../svg';
+import { useSelector } from 'react-redux';
+// Add profile picture from data
+export { useSelector } from 'react-redux';
 
 export default function Header() {
+  const { user } = useSelector((user) => ({ ...user }));
   const color = '#65676b';
+
   return (
     <header>
+      {/* LOGO SECTION */}
       <div className="header_left">
         <Link to="/" className="header_logo">
           <div className="circle">
@@ -26,6 +36,7 @@ export default function Header() {
           <input type="text" className="hide_input" placeholder="Search" />
         </div>
       </div>
+      {/* MIDDLE MENU */}
       <div className="header_middle">
         <Link to="/" className="middle_icon hover1 active" color={color}>
           <HomeActive />
@@ -48,7 +59,26 @@ export default function Header() {
           <Gaming />
         </Link>
       </div>
-      <div className="header_right"></div>
+      {/* PROFILE SECTION 🔴 USER HTML CSS*/}
+      <div className="header_right">
+        <Link to="/profile" className="profile_link hove1">
+          <img src={user?.picture} alt="" />
+          <span>{user?.first_name}</span>
+        </Link>
+        <div className="circle_icon hover1">
+          <Menu />
+        </div>
+        <div className="circle_icon hover1">
+          <Messenger /> 
+        </div>
+        <div className="circle_icon hover1">
+          <Notifications />
+          <div className="right_notification">9</div>
+        </div>
+        <div className="circle_icon hover1">
+          <ArrowDown />
+        </div>
+      </div>
     </header>
   );
 }

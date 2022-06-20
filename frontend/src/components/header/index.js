9fa1f7a195296721f6bca2fceabf74435ca32f46
TraderCodes <TraderCodes@users.noreply.github.com>
2022-06-20 10:18:02 -0700

    Html css for seach + logo

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+import { Logo, Search } from '../../svg';
 
 export default function Header() {
+  const color = '#65676b';
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div className="header_left">
+        <Link to="/" className="header_logo">
+          <div className="circle">
+            <Logo />
+          </div>
+        </Link>
+        <div className="search search1">
+          <Search color={color} />
+          <input type="text" className="hide_input" placeholder="Search" />
+        </div>
+      </div>
+      <div className="header_middle"></div>
+      <div className="header_right"></div>
+    </header>
+  );
 }

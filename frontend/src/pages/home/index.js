ee8fe6dafe45368dc01ffb21c881d798771cdc8c
TraderCodes <TraderCodes@users.noreply.github.com>
2022-06-20 09:44:52 -0700

    Header-beginning

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,3 +1,10 @@
+import Header from "../../components/header";
+
 export default function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
 }
+

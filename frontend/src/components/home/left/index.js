396ddfd91e5f8c96d2d7ca30c00ae94f05c76635
TraderCodes <TraderCodes@users.noreply.github.com>
2022-06-23 10:06:31 -0700

    Added : shortcut + showmore toggle

--- frontend/src/components/home/left/index.js
+++ frontend/src/components/home/left/index.js
@@ -2,9 +2,13 @@ import LeftLink from './LeftLink';
 import './style.css';
 import { left } from '../../../data/home';
 import { Link } from 'react-router-dom';
+import { ArrowDown1 } from '../../../svg';
+import { useState } from 'react';
+import Shortcut from './Shortcut';
 export default function LeftHome({ user }) {
+  const [visible, setVisible] = useState(false);
   return (
-    <div className="left_home">
+    <div className="left_home scrollbar">
       <Link to="/profile" className="left_link hover1">
         <img src={user?.picture} alt="" />
         <span>
@@ -19,6 +23,55 @@ export default function LeftHome({ user }) {
           notification={link.notification}
         />
       ))}
+      {!visible && (
+        <div className="left_link hover1" onClick={() => setVisible(true)}>
+          <div className="small_circle">
+            {/* 🔴 EXTEND */}
+            <ArrowDown1 />
+          </div>
+          <span> Show more</span>
+        </div>
+      )}{' '}
+      {visible && (
+        <div className="more_left">
+          {left.slice(0, left.length).map((link, i) => (
+            <LeftLink
+              key={i}
+              img={link.img}
+              text={link.text}
+              notification={link.notification}
+            />
+          ))}
+          <div className="left_link hover1" onClick={() => setVisible(false)}>
+            <div className="small_circle rotate180">
+              <ArrowDown1 />
+            </div>
+            <span>Show less</span>
+          </div>
+        </div>
+      )}
+      <div className="splitter"></div>
+      <div className="shortcut">
+        <div className="heading">ShortCuts</div>
+        <div className="edit_shortcut">edit</div>
+      </div>
+      <div className="shortcut_list">
+        <Shortcut
+          link="https://github.com/TraderCodes"
+          img="frontend\public\images\github.png"
+          name="github"
+        />
+        <Shortcut
+          link="https://github.com/TraderCodes"
+          img="../../images/github.svg"
+          name="github"
+        />
+        <Shortcut
+          link="https://github.com/TraderCodes"
+          img="../../images/ytb.png"
+          name="Youtube"
+        />
+      </div>
     </div>
   );
 }

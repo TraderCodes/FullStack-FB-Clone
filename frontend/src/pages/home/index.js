76104489b1aebba4e8cc0bfc6d2ba9cbfd31619d
TraderCodes <TraderCodes@users.noreply.github.com>
2022-06-24 12:50:27 -0700

    Added html for creatpost comp

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -4,8 +4,9 @@ import Header, { useSelector } from '../../components/header';
 import useClickOutside from '../../helpers/clickOutside';
 import LeftHome from '../../components/home/left';
 import RightHome from '../../components/home/right';
-import Stories from '../../components/home/stories/index'
-import "./style.css" 
+import Stories from '../../components/home/stories/index';
+import './style.css';
+import CreatePost from '../../components/createPost';
 
 export default function Home() {
   const { user } = useSelector((user) => ({ ...user }));
@@ -16,12 +17,13 @@ export default function Home() {
     console.log('ga');
   });
   return (
-    <div className="home" >
+    <div className="home">
       <Header />
       {/* {visible && <div className="card" ref={el}></div>} */}
       <LeftHome user={user} />
       <div className="home_middle">
-        <Stories/>
+        <Stories />
+        <CreatePost user={user} />
       </div>
       <RightHome user={user} />
     </div>

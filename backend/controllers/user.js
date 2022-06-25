55521c4604a194e9f9868da9826133202b7dc319
TraderCodes <TraderCodes@users.noreply.github.com>
2022-06-25 08:03:43 -0700

    Add : UserAuth middleware

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -146,3 +146,7 @@ exports.login = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.auth = (req, res) => {
+  res.json('welcome auth');
+};
---
+++ backend/middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+exports.authUser = async (req, res, next) => {
+  try {
+    let tmp = req.header('Authorization');
+    // add bearer in front of token for sercurity
+    // only get the token , so we slice Bearer
+    const token =tmp? tmp.slice(7, tmp.length):"";
+    // if not token then invalid
+    if (!token) {
+      return res.status(400).json({ error: 'Invalid Authentication' });
+    }
+    jwt.verify(token,process.env.TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.status(400).json({ error: 'Invalid Authentication' });
+      }
+      // if is valid
+      // Add user id into user object
+      req.user = user;
+      next();
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};

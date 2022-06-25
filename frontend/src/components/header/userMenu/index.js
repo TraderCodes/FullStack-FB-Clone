0a452c62b14e003f94d31222d448a8bb08175f81
TraderCodes <TraderCodes@users.noreply.github.com>
2022-06-25 15:07:49 -0700

    Feature : Logout

--- frontend/src/components/header/userMenu/index.js
+++ frontend/src/components/header/userMenu/index.js
@@ -1,10 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import SettingsPrivacy from './SettingsPrivacy';
 import HelpSupport from './HelpSupport';
 import DisplayAccessibility from './DisplayAccessibility';
+import { useDispatch } from 'react-redux';
+import Cookies from 'js-cookie';
 export default function UserMenu({ user }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
   const [visible, setVisible] = useState(0);
+  const logout = () => {
+    // set cookies to none 
+    Cookies.set('user','')
+    dispatch({
+      type: 'LOGOUT',
+    });
+    navigate('/login')
+  };
+
   return (
     <div className="mmenu">
       {visible === 0 && (
@@ -73,7 +86,12 @@ export default function UserMenu({ user }) {
               <i className="right_icon"></i>
             </div>
           </div>
-          <div className="mmenu_item hover3">
+          <div
+            className="mmenu_item hover3"
+            onClick={() => {
+              logout();
+            }}
+          >
             <div className="small_circle">
               <i className="logout_filled_icon"></i>
             </div>

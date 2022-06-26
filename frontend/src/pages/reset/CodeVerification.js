a0d656c71a9f0e36d5876c4e92068252864d137a
TraderCodes <TraderCodes@users.noreply.github.com>
2022-06-26 11:00:36 -0700

    Add Yup to Code Verifiction page

--- frontend/src/pages/reset/CodeVerification.js
+++ frontend/src/pages/reset/CodeVerification.js
@@ -15,9 +15,15 @@ export default function CodeVerification({
   userInfos,
 }) {
 
+    const validateCode = Yup.object({
+      code: Yup.string()
+        .required('Code is required')
+        .min('5', 'Code must be 5 characters.')
+        .max('5', 'Code must be 5 characters.'),
+    });
   return (
     <div className="reset_form">
-      <div className="reset_form_header">Code verification</div>
+      <div className="reset_form_header">Code verification </div>
       <div className="reset_form_text">
         Please enter code that been sent to your email.
       </div>
@@ -26,7 +32,7 @@ export default function CodeVerification({
         initialValues={{
           code,
         }}
-        // validationSchema={validateCode}
+        validationSchema={validateCode}
         // onSubmit={() => {
         //   verifyCode();
         // }}
@@ -37,7 +43,7 @@ export default function CodeVerification({
               type="text"
               name="code"
               onChange={(e) => setCode(e.target.value)}
-              placeholder="Code"
+              placeholder="5 Number Code"
             />
             {error && <div className="error_text">{error}</div>}
             <div className="reset_form_btns">

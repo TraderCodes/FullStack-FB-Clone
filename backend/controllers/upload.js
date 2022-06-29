1c76ca0841631f3f8aa9f264249ba574c5a64a1a
TraderCodes <TraderCodes@users.noreply.github.com>
2022-06-29 11:20:15 -0700

    install cloudinary

--- backend/controllers/upload.js
+++ backend/controllers/upload.js
@@ -1,9 +1,10 @@
 const Post = require('../models/Post');
+const cloudinary = require('cloudinary')
 exports.uploadImages = async (req, res) => {
   try {
     // save to post in module
     
-    res.json('image upload');
+    res.json('image uploaded');
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }

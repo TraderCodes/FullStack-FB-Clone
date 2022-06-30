558cad88b21aa64ad46cfbe4895c6d943daa7fb4
TraderCodes <TraderCodes@users.noreply.github.com>
2022-06-30 16:56:49 -0700

    Set get req getallpost routes and controllers

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -4,6 +4,8 @@ const { authUser } = require('../middlewares/auth');
 const router = express.Router();
 
 router.post('/createPost',authUser,createPost);
-router.get('/getAllPosts',authUser,getAllPosts);
+
+router.get('/getAllPosts', getAllPosts);
+
 // router.post('/auth',authUser, auth);
 module.exports = router;

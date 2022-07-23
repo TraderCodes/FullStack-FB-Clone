9250ecd33af882c85791aa4a760009d179a91b94
TraderCodes <TraderCodes@users.noreply.github.com>
2022-07-23 14:14:04 -0700

    Add Friends menu

--- frontend/src/pages/profile/Friendship.js
+++ frontend/src/pages/profile/Friendship.js
@@ -1,4 +1,9 @@
+import { useRef, useState } from "react";
+
 export default function Friendship() {
+  const [friendsMenu, setFriendsMenu] = useState(true);
+  const menu = useRef(null);
+
   const friendship = {
     friends: true,
     following: false,
@@ -6,15 +11,52 @@ export default function Friendship() {
     requestReceived: false,
   };
   return (
-    <div>
+    <div className="friendship">
       {
         // 🔴 If Have Friends
+
         friendship.friends ? (
           <div className="friends_menu_wrap">
             <button classNamde="gray_btn">
               <img src="../../../icons/friends.png" alt="friends" />
               <span>Friends</span>
             </button>
+            {friendsMenu && (
+              <div className="open_cover_menu" ref={menu}>
+                <div className="open_cover_menu_item hover1">
+                  <img src="../../../icons/favoritesOutline.png" alt="" />
+                  Favorites
+                </div>
+                <div className="open_cover_menu_item hover1">
+                  <img src="../../../icons/editFriends.png" alt="" />
+                  Edit Friend list
+                </div>
+                {friendship?.following ? (
+                  <div
+                    className="open_cover_menu_item hover1"
+                
+                  >
+                    <img src="../../../icons/unfollowOutlined.png" alt="" />
+                    Unfollow
+                  </div>
+                ) : (
+                  <div
+                    className="open_cover_menu_item hover1"
+           
+                  >
+                    <img src="../../../icons/unfollowOutlined.png" alt="" />
+                    Follow
+                  </div>
+                )}
+                <div
+                  className="open_cover_menu_item hover1"
+              
+                >
+                  <i className="unfriend_outlined_icon"></i>
+                  Unfriend
+                </div>
+              </div>
+            )}
           </div>
         ) : (
           ''

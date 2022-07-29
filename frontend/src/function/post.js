848ff280d0bc349236fb17f5a606f3aeac1c991b
TraderCodes <TraderCodes@users.noreply.github.com>
2022-07-28 23:12:33 -0700

    add reactpost to get data from back end

--- frontend/src/function/post.js
+++ frontend/src/function/post.js
@@ -25,6 +25,27 @@ export const createPost = async (
     );
     return 'ok';
   } catch (error) {
+    return error.response.data.message; 
+  }
+};
+export const reactPost = async (
+ postId,react,token
+) => {
+  try {
+    const { data } = await axios.post(
+      `${process.env.REACT_APP_BACKEND_URL}/reactPost`,
+      {
+        postId,
+        react,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return 'ok';
+  } catch (error) {
     return error.response.data.message;
   }
 };

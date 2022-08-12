d1f7de59d40cf59cae0afed0dd2f9f81b8359276
TraderCodes <TraderCodes@users.noreply.github.com>
2022-08-12 00:40:02 -0700

    display user options after user input search

--- frontend/src/components/header/searchMenu.js
+++ frontend/src/components/header/searchMenu.js
@@ -1,9 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { search } from '../../function/user';
 import useClickOutside from '../../helpers/clickOutside';
 import { Return, Search } from '../../svg';
 import './style.css';
-export default function SearchMenu({ color, setShowSearchMenu,token }) {
+export default function SearchMenu({ color, setShowSearchMenu, token }) {
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
   const menu = useRef(null);
@@ -17,15 +18,15 @@ export default function SearchMenu({ color, setShowSearchMenu,token }) {
     setShowSearchMenu(false);
   });
   const searchHandler = async () => {
-    if(searchTerm === ''){
-      setResults('')
+    if (searchTerm === '') {
+      setResults('');
     }
     // else send data to the backend
-    else{
-      const res = await search(searchTerm,token)
-      setResults(res)
+    else {
+      const res = await search(searchTerm, token);
+      setResults(res);
     }
-  }
+  };
   console.log(results);
   return (
     <div className="header_left search_area scrollbar " ref={menu}>
@@ -58,9 +59,20 @@ export default function SearchMenu({ color, setShowSearchMenu,token }) {
         <span>Recent Search</span>
         <a> edit</a>
       </div>
-      <div className
-      ="search_history"></div>
-      <div className="search_results scrollbar"></div>
+      <div className="search_history"></div>
+      <div className="search_results scrollbar">
+        {results &&
+          results.map((user) => (
+            <Link to={`/profile/${user.username}`} className="search_user_item">
+              <img src={user.picture} alt="" />
+              <span>
+                {user.first_name}
+                {user.last_name}
+              </span>
+            </Link>
+            
+          ))}
+      </div>
     </div>
   );
 }
